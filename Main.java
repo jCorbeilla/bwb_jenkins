9661ba1c272baf8423856fdd5d2f1f59c0ff4336
jCorbeilla <BWB.j.corbeilla@fondespierre.com>
2022-08-24 10:30:59 +0200

    ADD Main

---
+++ Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Jeremy Corbeilla <jeremy.corbeilla@fairfair.com>
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello dear !!!");
+    }
+
+}
